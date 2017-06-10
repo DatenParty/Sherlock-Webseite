25ecde25048c6b379af9d2db5c0ae3a75232346c
JanMoritzMeyer <janmoritzmeyer@yahoo.com>
2017-06-10 16:34:16 +0200

    COLOR

--- newWEB/function.php
+++ newWEB/function.php
@@ -2,7 +2,7 @@
 $json=file_get_contents( "http://maschini.de:5001/"); 
 $test=json_decode($json, true);
 foreach($test as $testt){
-    echo $testt["author"];
+    echo $testt["article"];
 }
 ?>
 <style>
@@ -18,7 +18,6 @@ foreach($test as $testt){
     }
 </style>
     <img class="left" src="img.jpg" />
-<div class="right">
     <h2>Trump zu Aussage unter Eid bereit</h2>
     <h5>Stand 10.06.2017 06:51 Uhr</h5>
     <h5>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</h5>

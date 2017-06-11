6c0110dc9c2e47a6c0be732c1ccf02b677f8e8b3
JanMoritzMeyer <janmoritzmeyer@yahoo.com>
2017-06-11 09:37:14 +0200

    CATEGORY
    Category Changes

--- newWEB/index.php
+++ newWEB/index.php
@@ -5,17 +5,40 @@ echo file_get_contents("header.inc.php");
         <?php
         $json = file_get_contents( "http://maschini.de:5001/alt");
         $file = json_decode($json, true);
-        foreach ($file as $entry) {
-            echo "<table align=\"center\" width=\"1000px\" style='padding: 50px 0'>";
-            echo "<tr>";
-            echo "<td class=\"image\"><img src=\"" . ($entry["imglink"] == "" || $entry["author"] == "FAZ" ? "../black.jpeg" : $entry["imglink"]) . "\" width=\"300px\" height=\"200px\"></td>";
-            echo "<td class=\"content\">";
-            echo "<h2><a href=\"" . $entry["link"] . "\" target='blank'>" . $entry["heading"] . "</a></h2>";
-            echo "<span>" . $entry["date"] . " " . $entry["author"] . "</span><br>";
-            echo "<div class=\"text\">" . $entry["article"] . "</div>";
-            echo "</td>";
-            echo "</tr>";
-            echo "</table>";
+        $category = $_GET["category"];
+        if(isset($_GET["category"])){
+            foreach ($file as $entry) {
+                    if($_GET["category"] == $entry["category"]){
+                        echo "<table align=\"center\" width=\"1000px\" style='padding: 50px 0'>";
+                        echo "<tr>";
+                        echo "<td class=\"image\"><img src=\"" . ($entry["imglink"] == "" || $entry["author"] == "FAZ" ? "../black.jpeg" : $entry["imglink"]) . "\" width=\"300px\" height=\"200px\"></td>";
+                        echo "<td class=\"content\">";
+                        echo "<h2><a href=\"" . $entry["link"] . "\" target='blank'>" . $entry["heading"] . "</a></h2>";
+                        echo "<span>" . $entry["date"] . " " . $entry["author"] . "</span><br>";
+                        echo "<div class=\"text\">" . $entry["article"] . "</div>";
+                        echo "</td>";
+                        echo "</tr>";
+                        echo "</table>";
+                    }
+                else{
+                    
+                }
+                
+            }
+        }
+        else{
+            foreach ($file as $entry) {
+                echo "<table align=\"center\" width=\"1000px\" style='padding: 50px 0'>";
+                echo "<tr>";
+                echo "<td class=\"image\"><img src=\"" . ($entry["imglink"] == "" || $entry["author"] == "FAZ" ? "../black.jpeg" : $entry["imglink"]) . "\" width=\"300px\" height=\"200px\"></td>";
+                echo "<td class=\"content\">";
+                echo "<h2><a href=\"" . $entry["link"] . "\" target='blank'>" . $entry["heading"] . "</a></h2>";
+                echo "<span>" . $entry["date"] . " " . $entry["author"] . "</span><br>";
+                echo "<div class=\"text\">" . $entry["article"] . "</div>";
+                echo "</td>";
+                echo "</tr>";
+                echo "</table>";
+            }
         }
         ?>
 	  <script>

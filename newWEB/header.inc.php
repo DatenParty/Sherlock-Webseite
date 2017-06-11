aa6ed946e4b315bfcc254b9c55ea5f1ba7362094
JanMoritzMeyer <janmoritzmeyer@yahoo.com>
2017-06-11 10:28:44 +0200

    Design Menu

--- newWEB/header.inc.php
+++ newWEB/header.inc.php
@@ -21,13 +21,13 @@
         <center>
             <nav>
 				<li>Themen<ul>
-					<li><a href="index.php?category=Sport">Sport</a></li>
-					<li><a href="index.php?category=Kultur">Kultur</a></li>
-					<li><a href="index.php?category=Politik">Politik</a></li>
-					<li><a href="index.php?category=Finanzen">Finanzen</a></li>
-					<li><a href="index.php?category=Unterhaltung">Unterhaltung</a></li>
-					<li><a href="index.php?category=Digital">Digital</a></li>
-					<li><a href="index.php?category=Lifestyle">Lifestyle</a></li>
+					<li><a href="index.php?category=Sport" class="links">Sport</a></li>
+					<li><a href="index.php?category=Kultur" class="links">Kultur</a></li>
+					<li><a href="index.php?category=Politik" class="links">Politik</a></li>
+					<li><a href="index.php?category=Finanzen" class="links">Finanzen</a></li>
+					<li><a href="index.php?category=Unterhaltung" class="links">Unterhaltung</a></li>
+					<li><a href="index.php?category=Digital" class="links">Digital</a></li>
+					<li><a href="index.php?category=Lifestyle" class="links">Lifestyle</a></li>
 					<li>Weiteres</li>
 					</ul></li>
 					<li><a class="link" href="index.php">Aktuell</a></li>

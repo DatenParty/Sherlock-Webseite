571a70c815fa633b44977ee15e80cc8b74b3c85d
RyanMacCain <ufufigugkb367@gmail.com>
2017-06-11 11:12:21 +0200

    k
    p

--- newWEB/datenparty-forum.php
+++ newWEB/datenparty-forum.php
@@ -1,3 +1,429 @@
 <?php
 echo file_get_contents("header.inc.php");
 ?>
+	<div class="forumheader">
+		Überschrift 1
+	</div>
+	<ul class="forumposts">
+		<li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li>
+		<li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li>
+	</ul>
+	<div class="forumheader">
+		Überschrift 1
+	</div>
+	<ul class="forumposts">
+		<li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li>
+		<li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li>
+	</ul>	<div class="forumheader">
+		Überschrift 1
+	</div>
+	<ul class="forumposts">
+		<li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li>
+		<li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li>
+	</ul>	<div class="forumheader">
+		Überschrift 1
+	</div>
+	<ul class="forumposts">
+		<li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li>
+		<li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li><li class="forumpost">
+			<span class="forumposttitle">
+				gbrgbr
+			</span>
+			<span class="forumpostauthor">
+				Algebra heol
+			</span>
+			<span class="forumpostdate">
+				07.03.1967
+			</span>
+		</li>
+	</ul>
